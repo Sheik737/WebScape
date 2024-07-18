0aab3c2c16a5b0f8f32e0e40b8021e1404621b02
Sheik Zubair <zsheik436@gmail.com>
2024-07-18 09:13:39 +0530

    the navbar is now element aware

--- script.js
+++ script.js
@@ -2,23 +2,49 @@ const hamMenu = document.querySelector(".ham-menu");
 const menuContainer = document.querySelector('.nav-container')
 const Menu = document.querySelector(".links");
 const link = document.querySelectorAll(".link");
-
-
 const ansMenu = document.querySelectorAll('.answerToggle')
+const sections = document.querySelectorAll('.section');
+const navLinks = document.querySelectorAll('.nav-container .link');
+// console.log(sections)
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        const index = Array.from(sections).indexOf(entry.target);
+        const navLink = navLinks[index];
+
+        if (entry.isIntersecting) {
+            navLink.classList.add('active');
+        } else {
+            navLink.classList.remove('active');
+        }
+    });
+}, options);
+
+sections.forEach(section => {
+    observer.observe(section);
+});
+
+
+
 
 hamMenu.addEventListener("click", () => {
     hamMenu.classList.toggle("active");
     Menu.classList.toggle("active");
     menuContainer.classList.toggle("active");
     link.forEach(element => {
-            element.classList.toggle('active')         
+        element.classList.toggle('active')
     });
 });
 
 ansMenu.forEach(element => {
-    element.addEventListener("click",()=>{
+    element.addEventListener("click", () => {
         element.parentElement.nextElementSibling.classList.toggle('active')
         element.classList.toggle('active')
     })
-    
+
 });
